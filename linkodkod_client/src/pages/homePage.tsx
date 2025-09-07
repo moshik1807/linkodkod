c5c3d2fbe667218e6a384d7fb6b5c6fe37104f6e
moshik <moshik0527669169@gmail.com>
2025-09-07 16:52:21 +0300

    added header

--- linkodkod_client/src/pages/homePage.tsx
+++ linkodkod_client/src/pages/homePage.tsx
@@ -1,9 +1,11 @@
 import Post from "../components/postComponent"
 import postJson from "../posts.json"
+import Header from "../components/headerComponent"
 import "../syles/homeStyle.css"
 export default function Home(){
     return(
         <>
+        <Header imgLogo="../../public/image.png" slogan="Linkodkod"/>
         <main>
             {postJson.map(p=>(
                 <Post imgSrc={p.imgSrc} description={p.description} authorName={p.authorName} amountOfLikes={p.amountOfLikes}/>
