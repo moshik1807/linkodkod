e2cd3065b1773b3dd7b08d2c0d0396a8a613274e
moshik <moshik0527669169@gmail.com>
2025-09-08 12:54:44 +0300

    Adding loading posts from the server

--- linkodkod_client/src/pages/homePage.tsx
+++ linkodkod_client/src/pages/homePage.tsx
@@ -1,13 +1,34 @@
 import Post from "../components/post/postComponent"
-import postJson from "../posts.json"
+// import postJson from "../posts.json"
 import "../syles/homeStyle.css"
 import Layout from "../components/application-layout/layout"
+import { useState,useEffect } from "react"
+
 export default function Home(){
+        type Post = {
+        imgSrc: string;
+        description: string;
+        authorName:string;
+        amountOfLikes:number
+    };
+
+    const[posts,setPosts]=useState<Post[]>([]);
+
+
+    useEffect(() => {
+        fetch("http://localhost:3000/getAll")
+            .then((res)=> res.json())
+            .then((data)=> setPosts(JSON.parse(data)))
+            .catch((err)=> console.log("Error fetching riddles:", err))
+    }, []);
+    console.log(posts)
+
+
     return(
         <>
         <Layout>
         <main>
-            {postJson.map(p=>(
+            {posts.map(p=>(
                 <Post imgSrc={p.imgSrc} description={p.description} authorName={p.authorName} amountOfLikes={p.amountOfLikes}/>
             ))}
         </main>
