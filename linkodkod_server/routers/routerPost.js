bc428591cc25dcb5cb5c27128df1b4c813ce89be
moshik <moshik0527669169@gmail.com>
2025-09-09 12:07:56 +0300

    added create in server

--- linkodkod_server/routers/routerPost.js
+++ linkodkod_server/routers/routerPost.js
@@ -1,12 +1,14 @@
 import express from "express"
 import  {getAllPosts}  from "../controllers/controllerPost.js"
 import  {getPostById}  from "../controllers/controllerPost.js"
-
+import { createPost } from "../controllers/controllerPost.js"
 const routerPost = express.Router()
 
 routerPost.get('/getAll',getAllPosts)
 
 routerPost.get("/getById/:id",getPostById)
+
+routerPost.post("/create",createPost)
 
 export default routerPost
 
--- linkodkod_server/services/postService.js
+++ linkodkod_server/services/postService.js
@@ -1,4 +1,4 @@
-import fs from "fs/promises"
+import * as fs from "fs/promises"
 
 export async function readPosts(){
     try {
@@ -12,9 +12,8 @@ export async function readPosts(){
 
 
 export async function getPostByID(id){
-    const data = await fs.readFile("./posts.json", "utf8");    
+    const data = await fs.readFile("./posts.json", "utf8")    
     const post = JSON.parse(data).filter((e) => e.id == id)
-    console.log(data)
     console.log(post)
     if(post.length){
         return post[0]
@@ -23,3 +22,29 @@ export async function getPostByID(id){
     }
 }
 
+export async function newPost(post) {
+    try{
+        const data = JSON.parse(await fs.readFile("./posts.json", "utf8"))
+        post.id = data[data.length - 1].id + 1
+        data.push(post)
+        const newData = JSON.stringify(data)
+        await fs.writeFile("./posts.json", newData, 'utf-8',null,2)
+    }catch(err){
+        console.log(err)
+    }
+}
+
+// const y = {
+//     "a":1,
+//     "b":2
+// }
+
+
+// async function x(y){
+//     fetch('http://localhost:3000/create', {
+//         method: 'POST',
+//         headers: {'Content-Type': 'application/json',},
+//         body: JSON.stringify(y)
+//         })
+// }
+// await x(y)
